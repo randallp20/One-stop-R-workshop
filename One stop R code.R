e15674c778bd256bc3039e22e0a86b2a6d17126f
Randy Coryell <coryellr@nmsu.edu>
2016-06-09 09:26:45 -0600

    4th

--- One stop R code.R
+++ One stop R code.R
@@ -4,4 +4,23 @@ gold.price.per.ounce
 gold.price.per.ounce <-1150.00
 gold.price.per.ounce
 my.investment.amount<-10000
-my.investment.amount
+
+
+## slide 2
+panhandler.name <- "David"
+panhandler.name
+make.a.million.dollars <- TRUE
+make.a.million.dollars
+too.little.too.late <-FALSE
+too.little.too.late
+
+## slide 3
+my.venture.capitalists<-
+  c("John", "Mary", "Stephanie", "Maurice")
+my.venture.capitalists
+vc.investment.stake <-
+  c(20000,25000,15000,10000)
+vc.investment.stake
+vc.has..mining.knowledge <-
+  c(TRUE,FALSE,TRUE,FALSE)
+vc.has..mining.knowledge
